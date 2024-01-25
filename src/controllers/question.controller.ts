b0422aead124d3687975e0574cc182492ab202db
hakuunabatata <emachadomartins1999@gmail.com>
2024-01-25 16:11:03 -0300

    create CreateQuestion controller

--- src/controllers/question.controller.ts
+++ src/controllers/question.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { z as zod } from 'zod'
 import { CurrentUser } from '../auth'
 import { ZodValidationPipe } from '../pipes'
 import { PrismaService } from '../services'
+import { slugify } from '../utils'
 
 const questionBodySchema = zod.object({
   content: zod.string(),
@@ -18,15 +19,14 @@ export class QuestionController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  @UsePipes(new ZodValidationPipe(questionBodySchema))
   async handle(
-    @Body() body: QuestionBodySchema,
-    @CurrentUser() user: { sub: string },
+    @Body(new ZodValidationPipe(questionBodySchema)) body: QuestionBodySchema,
+    @CurrentUser() { sub: authorId }: { sub: string },
   ) {
     const { content, title } = body
 
     const question = await this.prisma.question.create({
-      data: { content, title, slug: '', authorId: user.sub },
+      data: { content, title, slug: slugify(title), authorId },
     })
 
     return {
---
+++ src/utils/slug.ts
@@ -0,0 +1,7 @@
+export const slugify = (info: string): string =>
+  info
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^\w\s-]/g, '')
+    .replace(/\s+/g, '-')
